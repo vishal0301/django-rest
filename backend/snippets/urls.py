11b85322d68887df089dfcbb1b0af25fcae98968
Vishal0301 <vishalvaibhav000@gmail.com>
2023-07-03 15:42:36 +0530

    views modified and router for viewsets

--- backend/snippets/urls.py
+++ backend/snippets/urls.py
@@ -1,59 +1,106 @@
-# from django.urls import path
-# from snippets import views
+# # from django.urls import path
+# # from snippets import views
 
-# urlpatterns = [
-#     path('snippets/', views.snippet_list),
-#     path('snippets/<int:pk>/', views.snippet_detail),
-# ]
+# # urlpatterns = [
+# #     path('snippets/', views.snippet_list),
+# #     path('snippets/<int:pk>/', views.snippet_detail),
+# # ]
+
+# # from django.urls import path
+# # from rest_framework.urlpatterns import format_suffix_patterns
+# # from snippets import views
+
+# # urlpatterns = [
+# #     path('snippets/', views.snippet_list),
+# #     path('snippets/<int:pk>/', views.snippet_detail),
+# # ]
+
+# # urlpatterns = format_suffix_patterns(urlpatterns)
+
+# # Using class based views
+
+# # from django.urls import path
+# # from rest_framework.urlpatterns import format_suffix_patterns
+# # from snippets import views
+
+# # urlpatterns = [
+# #     path('', views.api_root),
+# #     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+# #     path('users/', views.UserList.as_view()),
+# #     path('users/<int:pk>/', views.UserDetail.as_view()),
+# #     path('snippets/', views.SnippetList.as_view()),
+# #     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+# # ]
+
+# # urlpatterns = format_suffix_patterns(urlpatterns)
 
 # from django.urls import path
 # from rest_framework.urlpatterns import format_suffix_patterns
 # from snippets import views
 
-# urlpatterns = [
-#     path('snippets/', views.snippet_list),
-#     path('snippets/<int:pk>/', views.snippet_detail),
-# ]
+# # # API endpoints
+# # urlpatterns = format_suffix_patterns([
+# #     path('', views.api_root),
+# #     path('snippets/',
+# #         views.SnippetList.as_view(),
+# #         name='snippet-list'),
+# #     path('snippets/<int:pk>/',
+# #         views.SnippetDetail.as_view(),
+# #         name='snippet-detail'),
+# #     path('snippets/<int:pk>/highlight/',
+# #         views.SnippetHighlight.as_view(),
+# #         name='snippet-highlight'),
+# #     path('users/',
+# #         views.UserList.as_view(),
+# #         name='user-list'),
+# #     path('users/<int:pk>/',
+# #         views.UserDetail.as_view(),
+# #         name='user-detail')
+# #])
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+# from snippets.views import SnippetViewSet, UserViewSet, api_root
+# from rest_framework import renderers
 
-# Using class based views
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
 
-# from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patterns
-# from snippets import views
 
-# urlpatterns = [
-#     path('', views.api_root),
-#     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-#     path('users/', views.UserList.as_view()),
-#     path('users/<int:pk>/', views.UserDetail.as_view()),
-#     path('snippets/', views.SnippetList.as_view()),
-#     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-# ]
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
-
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
-])
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
