558faf5da90b24b52b86baf5ee2415e6e610a876
Vishal0301 <vishalvaibhav000@gmail.com>
2023-07-03 15:12:44 +0530

    Views modified and made possible for hyperlinked relationship

--- backend/snippets/serializers.py
+++ backend/snippets/serializers.py
@@ -31,20 +31,40 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 # Using ModelSeriaizer
 
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = Snippet
-        fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#     class Meta:
+#         model = Snippet
+#         fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
 
 
+
+
+# from django.contrib.auth.models import User
+
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'snippets']
 
 
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
