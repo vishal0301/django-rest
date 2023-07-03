07c0460a6c3e7d65dde24dff8fefe959f2cec853
Vishal0301 <vishalvaibhav000@gmail.com>
2023-07-03 14:59:48 +0530

    created permissions.py files and modified views and urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
 ]
--- backend/snippets/serializers.py
+++ backend/snippets/serializers.py
@@ -32,6 +32,19 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 # Using ModelSeriaizer
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+
+
+
+
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
